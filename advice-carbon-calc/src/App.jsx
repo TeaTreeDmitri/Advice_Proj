72d1866dbe22f68c90bafb537100230a9205e9b7
Gareth Davies <mrgarethddavies@gmail.com>
2022-11-21 10:49:15 +0100

    whoopsie

--- advice-carbon-calc/src/App.jsx
+++ advice-carbon-calc/src/App.jsx
@@ -4,7 +4,6 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
-}
+  return <div className="App"></div>;}
 
 export default App;

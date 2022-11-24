0eb5be7cc9660aedf386ca4a55cc1847d9a360e4
Rei Sikk <reix0010@stud.kea.dk>
2022-11-24 19:18:00 +0100

    units fixed

--- advice-carbon-calc/src/Components/DropboxField.jsx
+++ advice-carbon-calc/src/Components/DropboxField.jsx
@@ -1,17 +1,18 @@
 function DropboxField(props) {
   return (
     <label htmlFor="storage3">
-            Dropbox storage size in GB
-            <input
-              type="number"
-              name="dropbox-storage"
-              id="dropbox-storage"
-              required
-              placeholder="Enter a number"
-              onKeyUp={props.handleDropboxChange}
-            />
-          </label>
-  )
+      Dropbox storage size in GB
+      <input
+        type="number"
+        name="dropbox-storage"
+        id="dropbox-storage"
+        step=".01"
+        required
+        placeholder="Enter a number"
+        onKeyUp={props.handleDropboxChange}
+      />
+    </label>
+  );
 }
 
-export default DropboxField
+export default DropboxField;
--- advice-carbon-calc/src/Components/OnedriveField.jsx
+++ advice-carbon-calc/src/Components/OnedriveField.jsx
@@ -1,19 +1,18 @@
-
-
 function OnedriveField(props) {
   return (
     <label htmlFor="storage3">
-            Onedrive storage size in GB
-            <input
-              type="number"
-              name="onedrive-storage"
-              id="onedrive-storage"
-              required
-              placeholder="Enter a number"
-              onKeyUp={props.handleOnedriveChange}
-            />
-          </label>
-  )
+      Onedrive storage size in GB
+      <input
+        type="number"
+        name="onedrive-storage"
+        id="onedrive-storage"
+        step=".01"
+        required
+        placeholder="Enter a number"
+        onKeyUp={props.handleOnedriveChange}
+      />
+    </label>
+  );
 }
 
-export default OnedriveField
+export default OnedriveField;
